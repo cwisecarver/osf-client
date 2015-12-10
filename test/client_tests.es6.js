3cb63ba3898d908ea0a9867dce14264c8cacaec2
Chris Wisecarver <cwisecarver@cos.io>
2015-12-10 15:35:08 -0500

    renaming tests and adding new ones

---
+++ test/client_tests.es6.js
@@ -0,0 +1,11 @@
+import test from 'tape';
+import stuff from './tape_common.es6.js';
+
+test('Running client tests', (t) => {
+  t.equal(typeof stuff.client.nodes, 'function', 'client.nodes is a function');
+  t.equal(typeof stuff.client.users, 'function', 'client.users is a function');
+  t.equal(typeof stuff.client.files, 'function', 'client.files is a function');
+  t.equal(typeof stuff.client.session, 'object', 'client.session is an object');
+  t.equal(typeof stuff.client, 'object', 'client is an object');
+  t.end();
+});
