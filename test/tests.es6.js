9d97ce40e73ca8668c296b02bffe8cb9610587cc
Chris Wisecarver <cwisecarver@cos.io>
2015-12-10 15:34:50 -0500

    renaming test files

--- test/tests.es6.js
+++ test/tests.es6.js
@@ -1,2 +1,8 @@
-import * as bob from './tape_node_tests.es6.js';
-import * as sue from './tape_client_tests.es6.js';
+import * as client from './client_tests.es6.js';
+import * as comment from './comment_tests.es6.js';
+import * as node from './node_tests.es6.js';
+import * as registration from './registration_tests.es6.js';
+import * as user from './user_tests.es6.js';
+import * as log from './log_tests.es6.js';
+import * as file from './file_tests.es6.js';
+import * as collection from './collection_tests.es6.js';
