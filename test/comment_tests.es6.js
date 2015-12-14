b06c1cb6c980eb8eaf74edd380760d4cca7127bb
Chris Wisecarver <cwisecarver@cos.io>
2015-12-14 09:39:32 -0500

    Fixing comment relationships and adding user tests

--- test/comment_tests.es6.js
+++ test/comment_tests.es6.js
@@ -8,20 +8,18 @@ global.commentIds = [];
 test('Running authedClient comment tests', t => {
   authedClient.nodes().get({query:{embed:'comments', 'page[size]':100}}).then(resp => {
     _.each(resp.data, node => {
-      console.log(node.embeds);
       _.each(node.embeds.comments.data, comment => {
         global.commentIds.push(comment.id);
       });
     });
-    console.log(global.commentIds);
     _.each(global.commentIds, commentId => {
-      console.log('comment!');
       authedClient.comments(commentId).get().then(resp => {
         t.equal(typeof resp, 'object', 'The Response Object is an Object');
         t.true(resp.data.length, 'Registered User has Comments');
         t.equal(typeof resp.data[0].id, 'string', 'The Id is a String');
         t.equal(resp.data[0].id.length, 5, 'The Id is Five Characters');
         t.equal(resp.data[0].type, 'collections', 'The Collection is a Comment');
+        t.equal(typeof resp.data[0].relationships.self, 'object', 'The comment has a self relationship');
       });
     });
   });
--- test/user_tests.es6.js
+++ test/user_tests.es6.js
@@ -1,1 +1,37 @@
 // read, update
+import test from 'tape';
+import stuff from './common.es6.js';
+import _ from 'underscore';
+
+let client = stuff.client;
+let authedClient = stuff.authedClient;
+
+test('Running user tests', t => {
+  client.users().get().then(resp => {
+    t.equal(resp.data.length, 10, 'Response has ten users');
+    t.equal(typeof resp.data[0].id, 'string', 'User ID is a string');
+    t.equal(typeof resp, 'object', 'Response is an object');
+    t.equal(resp.data[0].id.length, 5, 'User ID is 5 characters');
+  });
+
+  authedClient.users('me').get().then(resp => {
+    t.equal(typeof resp, 'object', 'Response is an object');
+    let fullName = resp.data.attributes.full_name;
+    let userId = resp.data.id;
+    console.log(fullName);
+    authedClient.users(userId)
+      .update({body:{full_name: fullName.split("").reverse().join("")}}).then(resp => {
+         console.log(fullName);
+         console.log(resp.data.attributes.full_name);
+         t.equal(resp.data.attributes.full_name, fullName, 'Username matches');
+    });
+    authedClient.users(userId)
+      .update({body:{full_name: fullName.split("").reverse().join("")}}).then(resp => {
+         console.log(fullName);
+         console.log(resp.data.attributes.full_name);
+         t.equal(resp.data.attributes.full_name, fullName, 'Username matches');
+    });
+  });
+
+  t.end();
+});
